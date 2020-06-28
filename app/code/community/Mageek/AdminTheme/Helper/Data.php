eabe69b403f0126f33246939f1f01e72f137fe04
Jindra Hejlik <jindra@hejlik.cz>
2020-06-28 22:58:42 +0200

    v1.0.1 - Theme switch refactoring, minor CSS changes

--- app/code/community/Mageek/AdminTheme/Helper/Data.php
+++ app/code/community/Mageek/AdminTheme/Helper/Data.php
@@ -4,19 +4,21 @@
  * @category    Mageek
  * @package     Mageek_AdminTheme
  * @author      Jindra Hejlík <jindra@hejlik.cz>
- * @copyright   Copyright (c) Mageek (http://mageek.cz/)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright   Copyright (c) Mageek (http://hejlik.cz/)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
 class Mageek_AdminTheme_Helper_Data extends Mage_Core_Helper_Data
 {
 
-    const CONFIG_XML_PATH_THEME = 'admin/mageek_admintheme/theme_name';
-
-    public function getThemeName()
+    public function setAdminTheme()
     {
-        $theme = (string)Mage::getStoreConfig(self::CONFIG_XML_PATH_THEME);
-        $theme = trim($theme);
+
+        if (Mage::getStoreConfigFlag('admin/mageek_admintheme/theme_enable')) {
+            $theme = "mageek";
+        } else {
+            $theme = "default";
+        }
 
         return $theme;
     }
--- app/code/community/Mageek/AdminTheme/Model/Observer.php
+++ app/code/community/Mageek/AdminTheme/Model/Observer.php
@@ -4,8 +4,8 @@
  * @category    Mageek
  * @package     Mageek_AdminTheme
  * @author      Jindra Hejlík <jindra@hejlik.cz>
- * @copyright   Copyright (c) Mageek (http://mageek.cz/)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright   Copyright (c) Mageek (http://hejlik.cz/)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
 class Mageek_AdminTheme_Model_Observer
@@ -14,7 +14,7 @@ class Mageek_AdminTheme_Model_Observer
     public function selectedTheme()
     {
 
-        $theme = Mage::helper('mageek_admintheme')->getThemeName();
+        $theme = Mage::helper('mageek_admintheme')->setAdminTheme();
 
         if (!empty($theme)) {
             Mage::getDesign()->setArea('adminhtml')->setTheme($theme);
